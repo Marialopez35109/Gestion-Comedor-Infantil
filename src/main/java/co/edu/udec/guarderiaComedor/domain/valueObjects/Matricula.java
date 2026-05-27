ef8377525a143e3fd9ed788a83ef4f92bf9f438f
MariaLopez35109 <mlopezv1@unicartagena.edu.co>
2026-05-26 19:07:56 -0500

    creacion del value object Matricula

--- src/main/java/co/edu/udec/guarderiaComedor/domain/valueObjects/Matricula.java
+++ src/main/java/co/edu/udec/guarderiaComedor/domain/valueObjects/Matricula.java
@@ -1,4 +1,13 @@
 package co.edu.udec.guarderiaComedor.domain.valueObjects;
 
-public class Matricula {
+public record Matricula(String valor) {
+    public Matricula{
+        if (valor == null || valor.isBlank()) {
+            throw new IllegalArgumentException("La matricula no puede estar vacia");
+        }
+        if (!valor.matches("^[A-Z0-9]{3,10}$")){
+            throw new IllegalArgumentException("Matricula invalida, Usar 3-10 caracteres ");
+        }
+    }
+
 }
