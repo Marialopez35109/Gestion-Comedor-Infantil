9e9366cd57a54cbd316d703e1cdeb9acef03fa8f
MariaLopez35109 <mlopezv1@unicartagena.edu.co>
2026-05-26 21:18:13 -0500

    Creacion del valueobject Dni

--- src/main/java/co/edu/udec/guarderiaComedor/domain/valueObjects/Dni.java
+++ src/main/java/co/edu/udec/guarderiaComedor/domain/valueObjects/Dni.java
@@ -1,4 +1,9 @@
 package co.edu.udec.guarderiaComedor.domain.valueObjects;
 
-public class Dni {
+public record Dni(String valor) {
+    public Dni{
+        if(valor == null || valor.isBlank() || !valor.matches("\\d{8,10}[A-Z]?")){
+            throw new IllegalArgumentException("DNI invalido. Debe tener entr 8-10 digitos y letras opcionalmente.");
+        }
+    }
 }
