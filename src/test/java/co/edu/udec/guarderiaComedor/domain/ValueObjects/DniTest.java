4475e331a55f7063c9f8cf5271d908a5a9e5e2b1
MariaLopez35109 <mlopezv1@unicartagena.edu.co>
2026-05-28 00:19:32 -0500

    test(domain):FechaNacimientoTest de FechaNacimiento y DniTest de Dni

---
+++ src/test/java/co/edu/udec/guarderiaComedor/domain/ValueObjects/DniTest.java
@@ -0,0 +1,38 @@
+package co.edu.udec.guarderiaComedor.domain.ValueObjects;
+import co.edu.udec.guarderiaComedor.domain.ValueObejects.Dni;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
+
+class DniTest {
+    @Test
+    void debeCrearDniValido(){
+        Dni dni = new Dni("12345678");
+        assertThat(dni.valor()).isEqualTo("12345678");
+    }
+    @Test
+    void debeLanzarExcepcionSiDniNulo(){
+        assertThatThrownBy(()-> new Dni(null))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("DNI invalido.");
+    }
+    @Test
+    void debeLanzarExcepcionSiFormatoInvalido(){
+        assertThatThrownBy(()-> new Dni("1234a"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("Debe tener entr 8-10 digitos y letras opcionalmente.");
+    }
+    @Test
+    void debeLanzarExcepcionSiLongitudIncorrecta(){
+        assertThatThrownBy(()->new Dni("123"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("Debe tener entr 8-10 digitos y letras opcionalmente.");
+    }
+    @Test
+    void debeLanzarExcepcionSiDniVacio(){
+        assertThatThrownBy(()->new Dni(""))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("DNI invalido.");
+    }
+
+
+}
---
+++ src/test/java/co/edu/udec/guarderiaComedor/domain/ValueObjects/FechaNacimientoTest.java
@@ -0,0 +1,29 @@
+package co.edu.udec.guarderiaComedor.domain.ValueObjects;
+
+import co.edu.udec.guarderiaComedor.domain.ValueObejects.FechaNacimiento;
+import org.junit.jupiter.api.Test;
+import java.time.LocalDate;
+import static org.assertj.core.api.Assertions.*;
+
+public class FechaNacimientoTest {
+    @Test
+    void debeCrearFechaNacimientoValida(){
+        LocalDate fecha= LocalDate.of(2021,5,15);
+        FechaNacimiento fNacimiento = new FechaNacimiento(fecha);
+        assertThat(fNacimiento.valor()).isEqualTo(fecha);
+    }
+    @Test
+    void debeLanxarExcepcionSiFechaNula(){
+        assertThatThrownBy(()->new FechaNacimiento(null))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("La fecha no puede ser nula");
+    }
+    @Test
+    void debeLanzarExcepcionSiFechaFutura(){
+        LocalDate fechaFutura = LocalDate.now().plusDays(1);
+        assertThatThrownBy(()->new FechaNacimiento(fechaFutura))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("La fecha no puede ser futura a la fecha actual");
+    }
+
+}
