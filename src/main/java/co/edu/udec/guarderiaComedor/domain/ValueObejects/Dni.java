a26aec2c7ea53b26b13ab980b1b6540e9f74081b
MariaLopez35109 <mlopezv1@unicartagena.edu.co>
2026-05-28 00:20:55 -0500

    refactorizacion VO Dni

--- src/main/java/co/edu/udec/guarderiaComedor/domain/ValueObejects/Dni.java
+++ src/main/java/co/edu/udec/guarderiaComedor/domain/ValueObejects/Dni.java
@@ -2,8 +2,11 @@ package co.edu.udec.guarderiaComedor.domain.ValueObejects;
 
 public record Dni(String valor) {
     public Dni{
-        if(valor == null || valor.isBlank() || !valor.matches("\\d{8,10}[A-Z]?")){
-            throw new IllegalArgumentException("DNI invalido. Debe tener entr 8-10 digitos y letras opcionalmente.");
+        if(valor == null || valor.isBlank()){
+            throw new IllegalArgumentException("DNI invalido.");
+        }
+        if(!valor.matches("\\d{8,10}[A-Z]?")){
+            throw new IllegalArgumentException("Debe tener entr 8-10 digitos y letras opcionalmente.");
         }
     }
 }
