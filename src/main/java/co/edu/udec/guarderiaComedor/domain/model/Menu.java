aff1eab91be365ae0edf6cd8bfcf6ec750ffd7a7
MariaLopez35109 <mlopezv1@unicartagena.edu.co>
2026-05-30 20:49:26 -0500

    Entidad Menu, Nino

---
+++ src/main/java/co/edu/udec/guarderiaComedor/domain/model/Menu.java
@@ -0,0 +1,57 @@
+package co.edu.udec.guarderiaComedor.domain.model;
+import co.edu.udec.guarderiaComedor.domain.ValueObjects.MenuId;
+import co.edu.udec.guarderiaComedor.domain.ValueObjects.Plato;
+import java.util.*;
+
+public class Menu {
+    private final MenuId id;
+    private String nombre;
+    private List<Plato> platos;
+    private boolean activo;
+
+    public Menu(MenuId id, String nombre) {
+        this.id=Objects.requireNonNull(id, "El id no puede ser nulo");
+        this.nombre=Objects.requireNonNull(nombre, "El nombre no puede ser nulo");
+        this.platos=new ArrayList<>();
+        this.activo=true;
+    }
+    public void agregarPlato(Plato plato) {
+        this.platos.add(Objects.requireNonNull(plato, "El plato debe ser nulo"));
+    }
+    public void eliminarPLato(Plato plato) {
+        this.platos.remove(plato);
+    }
+    public void desactivar(){
+        this.activo=false;
+    }
+    public void activar(){
+        this.activo=true;
+    }
+
+    public MenuId getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public List<Plato> getPlatos() {
+        return Collections.unmodifiableList(platos);
+    }
+
+    public boolean isActivo() {
+        return activo;
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Menu menu = (Menu) o;
+        return Objects.equals(id, menu.id);
+    }
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
--- src/main/java/co/edu/udec/guarderiaComedor/domain/model/Nino.java
+++ src/main/java/co/edu/udec/guarderiaComedor/domain/model/Nino.java
@@ -1,22 +1,93 @@
 package co.edu.udec.guarderiaComedor.domain.model;
-
+import co.edu.udec.guarderiaComedor.domain.ValueObjects.*;
+import co.edu.udec.guarderiaComedor.domain.enums.EstadoNino;
 import java.time.LocalDate;
+import java.util.*;
 
 public class Nino {
-    private String matricula;
-    private String nombreCompleto;
-    private LocalDate fechaNacimiento;
-    private LocalDate fechaIngreso;
-    private String estado;
+    private final NinoId id;
+    private Matricula matricula;
+    private NombreCompleto nombreCompleto;
+    private FechaNacimiento fechaNacimiento;
+    private FechaIngreso fechaIngreso;
+    private LocalDate fechaBaja;
+    private EstadoNino estado;
+    private Set<Ingrediente> alergias;
 
-    public Nino(String matricula, String nombreCompleto, LocalDate fechaNcimiento, LocalDate fechaIngreso, String estado) {
-        this.matricula = matricula;
-        this.nombreCompleto = nombreCompleto;
-        this.fechaNacimiento = fechaNcimiento;
-        this.fechaIngreso = fechaIngreso;
-        this.estado = "Activo";
+    public Nino(NinoId id, Matricula matricula, NombreCompleto nombreCompleto, FechaNacimiento fechaNacimiento, FechaIngreso fechaIngreso) {
+        this.id=Objects.requireNonNull(id,"EL ID no puede ser nulo");
+        this.matricula = Objects.requireNonNull(matricula,"La matricula no puede ser nula");
+        this.nombreCompleto = Objects.requireNonNull(nombreCompleto,"El nombre no puede ser nulo");
+        this.fechaNacimiento = Objects.requireNonNull(fechaNacimiento,"La fecha de ingreso no puede ser nula");
+        this.fechaIngreso = Objects.requireNonNull(fechaIngreso,"La fecha de ingreso no puede ser nula");
+        this.estado = EstadoNino.ACTIVO;
+        this.fechaBaja=null;
+        this.alergias=new HashSet<>();
+    }
+    public void darDeBaja(LocalDate fecha) {
+        if (fecha==null)throw new IllegalArgumentException("La fecha de baja no puede ser nula");
+
+        if(this.estado==EstadoNino.INACTIVO){
+            throw new IllegalArgumentException("El niño ya se encuentra inactivo");
+        }
+        if(fecha.isBefore(this.fechaIngreso.valor())){
+            throw new IllegalArgumentException("La fecha de baja no puede ser anterior a la fecha de ingreso");
+        }
+        this.estado=EstadoNino.INACTIVO;
+        this.fechaBaja=fecha;
+    }
+    public void actualizarMatricula(Matricula nuevaMatricula){
+        this.matricula=Objects.requireNonNull(nuevaMatricula,"La nueva matricula no puede ser nula");
+    }
+    public void eliminarAlergia(Ingrediente ingrediente){
+        this.alergias.remove(ingrediente);
+    }
+    public boolean esAlergicoA(Ingrediente ingrediente){
+        return this.alergias.contains(ingrediente);
     }
 
+// getters
 
+    public NinoId getId() {
+        return id;
+    }
 
+    public Matricula getMatricula() {
+        return matricula;
+    }
+
+    public NombreCompleto getNombreCompleto() {
+        return nombreCompleto;
+    }
+
+    public FechaNacimiento getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public FechaIngreso getFechaIngreso() {
+        return fechaIngreso;
+    }
+
+    public LocalDate getFechaBaja() {
+        return fechaBaja;
+    }
+
+    public EstadoNino getEstado() {
+        return estado;
+    }
+
+    public Set<Ingrediente> getAlergias() {
+        return Collections.unmodifiableSet(alergias);
+    }
+    @Override
+    public boolean equals(Object o) {
+        if(this==o) return true;
+        if(o==null || getClass() != o.getClass()) return false;
+        Nino nino=(Nino)o;
+        return Objects.equals(id, nino.id);
+    }
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
