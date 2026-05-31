a55252b422c4679f2265b6968b88b228702f1ede
MariaLopez35109 <mlopezv1@unicartagena.edu.co>
2026-05-30 20:54:47 -0500

    Service RegistroNinoService

--- src/main/java/co/edu/udec/guarderiaComedor/domain/services/RegistroNinoService.java
+++ src/main/java/co/edu/udec/guarderiaComedor/domain/services/RegistroNinoService.java
@@ -1,4 +1,17 @@
 package co.edu.udec.guarderiaComedor.domain.services;
+import co.edu.udec.guarderiaComedor.domain.ValueObjects.Matricula;
+import co.edu.udec.guarderiaComedor.domain.model.Nino;
+import co.edu.udec.guarderiaComedor.domain.exceptions.NinoException;
 
 public class RegistroNinoService {
+    private final ValidadorMatriculaSpecification validadorUnicidad;
+
+    public RegistroNinoService(ValidadorMatriculaSpecification validadorUnicidad){
+        this.validadorUnicidad = validadorUnicidad;
+    }
+    public void registrarNino(Nino nino) throws NinoException{
+        if(!validadorUnicidad.esUnica(nino.getMatricula())){
+            throw new NinoException("La matricula" + nino.getMatricula().valor() + "ya existe en el sistema");
+        }
+    }
 }
