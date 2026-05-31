806f6aac5ce4721a5e6b4b7871eccab12e6f865f
MariaLopez35109 <mlopezv1@unicartagena.edu.co>
2026-05-30 20:53:06 -0500

    events NinoRegistradoEvent

--- src/main/java/co/edu/udec/guarderiaComedor/domain/events/NinoRegistradoEvent.java
+++ src/main/java/co/edu/udec/guarderiaComedor/domain/events/NinoRegistradoEvent.java
@@ -1,4 +1,18 @@
 package co.edu.udec.guarderiaComedor.domain.events;
+import java.time.Instant;
+import java.util.Objects;
 
-public class NinoRegistradoEvent {
+public record NinoRegistradoEvent(String ninoId, String matricula, Instant fechaOcurrencia){
+    public NinoRegistradoEvent(String ninoId, String matricula){
+        this(ninoId,matricula,Instant.now());
+    }
+    public NinoRegistradoEvent{
+        Objects.requireNonNull(ninoId, "El id del niño no puede ser nulo");
+        Objects.requireNonNull(matricula, "La matricula no puede ser nula");
+        Objects.requireNonNull(fechaOcurrencia, "La fecha del evento no puede ser nula");
+    }
 }
+
+
+
+
